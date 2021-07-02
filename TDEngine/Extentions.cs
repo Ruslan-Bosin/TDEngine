4d76e6c8a02f94016a0d28f5a683e22fa87963e2
Ruslan-Bosin <76877334+Ruslan-Bosin@users.noreply.github.com>
2021-07-02 23:39:24 +0300

    test

---
+++ TDEngine/Extentions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TDEngine {
+
+    static class Extentions
+    {
+
+        // Int Extentions:
+        public static int randomInt(this int self, int min, int max)
+        {
+            Random random = new Random();
+            int result = random.Next(min, max);
+            return result;
+        }
+
+    }
+}
--- TDEngine/Program.cs
+++ TDEngine/Program.cs
@@ -1,9 +1,35 @@
-﻿using System;
+﻿// System imports
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// SFML 
+using SFML.Graphics;
+using SFML.Window;
 
 namespace TDEngine {
+
     class Program {
+
+        static RenderWindow window;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            window = new RenderWindow(new VideoMode(800, 600), "Test");
+
+            window.Closed += (obj, e) => { window.Close(); };
+            window.Resized += (obj, e) => { window.SetView(new View(new FloatRect(0, 0, e.Width, e.Height))); };
+
+            while (window.IsOpen) {
+                window.DispatchEvents();
+
+                window.Display();
+            }
+
+            int number = 0;
+            Console.WriteLine("Random int is - " + number.randomInt(min: 0, max: 10).ToString());
         }
+
     }
+
 }
